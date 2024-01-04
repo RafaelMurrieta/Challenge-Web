9c2ca77b3854e4b36ac89704e21e44b50d9c0b54
Rafael Murrieta <r7121005846@gmail.com>
2024-01-03 22:38:37 -0600

    Nueva version de slice

--- scroll_animation/script.js
+++ scroll_animation/script.js
@@ -3,26 +3,26 @@ const container = document.getElementById('container')
 document.addEventListener('DOMContentLoaded', ()=>{
     for (var i = 0; i < 3; i++) {
         createElement(i)
+        move(i)
     }
     let contador = 3
-    // move()
 })
+
+
 contador = 3
 window.addEventListener('scroll',function(){
   let scrollPosition = window.scrollY;
   i = Math.round(scrollPosition)
-  i = i/5
-  if (i < 100) {
-        createElement(contador)
-        // move()
-        contador+=1
+  if (i < 130) {
+    createElement(contador)
+    move(contador)
+    contador+=1
   }
 })
 
 
 
 function createElement(i){
-    console.log(i);
     ElementDiv = document.createElement('div')
     spanD = document.createElement('span')
     spanD.textContent = 'Content'
@@ -35,35 +35,22 @@ function createElement(i){
     }
     ElementDiv.appendChild(spanD)
     container.appendChild(ElementDiv)
-    move()
 }
 
 
-function move(){
-    let izq = document.querySelectorAll('.elementD');
-    izq.forEach(element => {
-        let change = 500;
-        let i = parseInt(element.id);
-        if (i % 2 === 0){
-            let trans = document.getElementById(`${i}`);
-            let interval = setInterval(() => {
-                if (change >= 0) {
-                    trans.style.right = `${change}px`;
-                    change-=6;
-                } else {
-                    clearInterval(interval); // Detiene la animación cuando change llega a 0
-                }
-            }, 1); // Intervalo de tiempo para la animación
-        }else{
-            let trans = document.getElementById(`${i}`)
-            let interval = setInterval(() => {
-                if (change >= 0) {
-                    trans.style.left = `${change}px`;
-                    change-=6;
-                }else{
-                    clearInterval(interval)
-                }
-            }, 1);
-        }
-    });
+function move(id){
+    if (id % 2 == 0) {
+        setTimeout(() => {
+            trans = document.getElementById(`${id}`)
+            trans.className = 'zero'
+            trans.style.right = '0px'
+        }, 100);
+    }else{
+        setTimeout(() => {
+            trans = document.getElementById(`${id}`)
+            trans.className = 'zeroI'
+            trans.style.left = '0px'
+        }, 100);
+    }
 }
+
