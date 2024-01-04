054ad1ce0e1d5e57673a5733de260103a88cd895
Rafael Murrieta <r7121005846@gmail.com>
2024-01-03 18:49:23 -0600

    Animaciones, sin scroll

--- scroll_animation/script.js
+++ scroll_animation/script.js
@@ -1,13 +1,67 @@
 const container = document.getElementById('container')
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    for (let i = 0; i < 3; i++) {
-        ElementDiv = document.createElement('div')
-        spanD = document.createElement('span')
-        spanD.textContent = 'Content'
-        ElementDiv.className = 'elementD'
-        ElementDiv.appendChild(spanD)
-        container.appendChild(ElementDiv)
+    for (var i = 0; i < 3; i++) {
+        createElement(i)
     }
+    let contador = 3
+    // move()
 })
-{/* <div class="elementD"></div> */}
+
+window.addEventListener('scroll',function(){
+  let scrollPosition = window.scrollY;
+  i = Math.round(scrollPosition)
+  if (i > 130) {
+    contador+
+    createElement(contador)
+  }
+})
+
+
+
+function createElement(i){
+    console.log(i);
+    ElementDiv = document.createElement('div')
+    spanD = document.createElement('span')
+    spanD.textContent = 'Content'
+    ElementDiv.id = i
+    ElementDiv.className = 'elementD'
+    if (i%2==0) {
+        ElementDiv.style.right = '500px'
+    }else{
+        ElementDiv.style.left = '500px'
+    }
+    ElementDiv.appendChild(spanD)
+    container.appendChild(ElementDiv)
+    move()
+}
+
+
+function move(){
+    let izq = document.querySelectorAll('.elementD');
+    izq.forEach(element => {
+        let change = 500;
+        let i = parseInt(element.id);
+        if (i % 2 === 0){
+            let trans = document.getElementById(`${i}`);
+            let interval = setInterval(() => {
+                if (change >= 0) {
+                    trans.style.right = `${change}px`;
+                    change-=6;
+                } else {
+                    clearInterval(interval); // Detiene la animación cuando change llega a 0
+                }
+            }, 1); // Intervalo de tiempo para la animación
+        }else{
+            let trans = document.getElementById(`${i}`)
+            let interval = setInterval(() => {
+                if (change >= 0) {
+                    trans.style.left = `${change}px`;
+                    change-=6;
+                }else{
+                    clearInterval(interval)
+                }
+            }, 1);
+        }
+    });
+}
